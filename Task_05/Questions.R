a7f11193618206258e0e71d01fa6d9e5363c4f9c
journeecin <78055393+Journeecin@users.noreply.github.com>
2021-02-25 22:39:19 -0500

    journeewheeler Task_05

---
+++ Task_05/Questions.R
@@ -0,0 +1,6 @@
+1. Each stimulation begins ith 20 alleles. You can modify that by genetic drift.
+2. On average, 2.77 Ne generations. 
+3. The average is 23, the variance is 48. 
+4. Fitness helps show evolution trajectories that mirror patterns seen in nature. 
+5. no 
+
